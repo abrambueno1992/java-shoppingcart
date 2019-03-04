c7c6cac26c86fa3d7589cb65bd9de940b0f609df
Abraham Bueno <abueno@localhost.localdomain>
2019-03-04 12:30:39 -0600

    Commented out service file

--- src/main/java/com/abrahambueno/javashoppingcart/service/impl/UserServiceImpl.java
+++ src/main/java/com/abrahambueno/javashoppingcart/service/impl/UserServiceImpl.java
@@ -1,21 +1,9 @@
 package com.abrahambueno.javashoppingcart.service.impl;
 
-import com.abrahambueno.javashoppingcart.models.User;
-import com.abrahambueno.javashoppingcart.repositories.UserDao;
-import com.abrahambueno.javashoppingcart.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
 //@Service(value = "userService")
-public class UserServiceImpl implements UserDetailsService, UserService
-{
-
+//public class UserServiceImpl implements UserDetailsService, UserService
+//{
+//
 //    @Autowired
 //    private UserDao userDao;
 //
@@ -47,4 +35,4 @@ public class UserServiceImpl implements UserDetailsService, UserService
 //    {
 //        return userDao.save(user);
 //    }
-}
+//}
