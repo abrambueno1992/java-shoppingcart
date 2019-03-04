aa0868855ebacb9aace5b71d6e1f1b211b321a75
Abraham Bueno <abueno@localhost.localdomain>
2019-03-04 17:48:31 -0600

    Recovered class

--- src/main/java/com/abrahambueno/javashoppingcart/models/Cart.java
+++ src/main/java/com/abrahambueno/javashoppingcart/models/Cart.java
@@ -1,7 +1,46 @@
 package com.abrahambueno.javashoppingcart.models;
 
 import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
 public class Cart {
+
+    List<ProductList> cartItems = new ArrayList<ProductList>();
+
+    public void addProductList(int pid) {
+        ProductList product = getProductList(pid);
+        addToCart(product);
+    }
+
+    private ProductList getProductList(int pid){
+        ProductList product = null;
+        List<ProductList> products = new ProductLists().getProductLists(0);
+        for (ProductList prod: products){
+            if (prod.getPid() == pid){
+                product = prod;
+                break;
+            }
+        }
+        return product;
+    }
+
+    private void addToCart(ProductList product){
+        cartItems.add(product);
+    }
+
+    public void removeProductList(int pid){
+        ProductList prod = getProductList(pid);
+        cartItems.remove(prod);
+    }
+
+    void printCartItems(){
+        for(ProductList prod: cartItems){
+            System.out.println(prod.getName());
+        }
+    }
+
+
 }
