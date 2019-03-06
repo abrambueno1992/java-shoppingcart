09f7713ee20c882f482ae08d78879c712bf8e8b9
Sagar <saagardesai@gmail.com>
2019-03-05 19:13:56 -0600

    userController draft

--- src/main/java/com/abrahambueno/javashoppingcart/controllers/UserController.java
+++ src/main/java/com/abrahambueno/javashoppingcart/controllers/UserController.java
@@ -1,4 +1,46 @@
 package com.abrahambueno.javashoppingcart.controllers;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/users")
+public class UserController
+{
+    @Autowired
+    Private Userrepository userrepos;
+
+    @GetMapping("/users")
+    Public List<User> listAllUsers()
+    {
+        return userrepos.findall();
+    }
+
+    @PostMapping
+    public User addNewUser(@RequestBody User newuser) throws URISyntaxException
+    {
+        return userrepos.save(newuser)
+    }
+
+    @DeleteMapping("/users/{id}")
+    Public String deleteUserById(@PathVariable long id)
+    {
+        var founduser = userrepos.findById(id);
+        if (founduser.isPresent())
+        {
+            userrepos.deleteById(id);
+
+            return "{" + "\"id\":"   + foundUser.get().getId() +
+                    ",\"usename\":" + "\"" + foundUser.get().getUsername() + "\"" +
+                    ",\"role\":" + foundUser.get().getAuthority() + "}";
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
 }
