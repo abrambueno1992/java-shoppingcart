fe7f07f024918582e9826678a2ba995de0fa645e
Abraham Bueno <abueno@localhost.localdomain>
2019-03-06 17:52:48 -0600

    Changed the models and the CartController

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+-- insert into shoppingcartapp.product (productid, description, name, price, quantity)
+-- values (1, "pen", "black gel ink", 5, 200),
+--         (2, "pencil", "wood pencil", 0.25, 200),
+--         (3, "keyboard", "mechanical keyboard", 30, 250),
+--         (4, "mouse", "wireless mouse", 15, 250);
+--
+-- insert into shoppingcartapp.cartproducts(cartid, productid)
+-- values (1, 1),
+--         (1, 5),
+--         (1, 3),
+--         (1, 4);
+--
+-- insert into shoppingcartapp.cart(cartid, quantity)
+-- values (1, 0)
+
+
+null
