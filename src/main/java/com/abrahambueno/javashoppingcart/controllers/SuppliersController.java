39678f897944d726f9248698741df7e29eff4ed4
Sagar <saagardesai@gmail.com>
2019-03-05 20:12:49 -0600

    suppliersController minus products

--- src/main/java/com/abrahambueno/javashoppingcart/controllers/SuppliersController.java
+++ src/main/java/com/abrahambueno/javashoppingcart/controllers/SuppliersController.java
@@ -1,4 +1,44 @@
 package com.abrahambueno.javashoppingcart.controllers;
 
-public class SuppliersController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+import java.security.PublicKey;
+
+@RestController
+@RequestMapping("/suppliers")
+public class SuppliersController
+{
+    @Autowired
+    Private Supplierepository supplierrepos;
+
+    @GetMapping("/suppliers")
+    Public List<User> listAllSuppliers()
+    {
+        return supplierrepos.findall();
+    }
+
+    @PostMapping
+    public User addNewSupplier (@RequestBody Supplier newsupplier) throws URISyntaxException
+    {
+        return supplierrepos.save(newsupplier);
+    }
+
+    @DeleteMapping("/suppliers/{supplierid}")
+    Public String deleteSupplierById(@PathVariable long supplierid)
+    {
+        var foundsupplier = supplierrepos.findById(supplierid);
+        if (foundsupplier.isPresent())
+        {
+            supplierrepos.deleteById(supplierid);
+
+            return "{" + "\"supplierid\":"   + foundSupplier.get().getId() +
+                    ",\"usename\":" + "\"" + foundSupplier.get().getUsername();
+        }
+        else
+        {
+            return null;
+        }
+    }
 }
