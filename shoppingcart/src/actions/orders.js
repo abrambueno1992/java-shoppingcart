f1905dfdd02be137cea9fa52965b23b6cf0399ca
Abraham Bueno <abueno@localhost.localdomain>
2019-03-07 16:24:36 -0600

    Changes

--- shoppingcart/src/actions/orders.js
+++ shoppingcart/src/actions/orders.js
@@ -3,6 +3,8 @@ export const CREATE_CART = "CREATE_CART";
 export const ADD_ITEM_TO_CART = "ADD_ITEM_TO_CART";
 export const DELETE_CART = "DELETE_CART";
 export const NEW_ORDER = "NEW_ORDER";
+export const DELETE_ORDER = "DELETE_ORDER";
+
 const url = "http://localhost:2019/orders/";
 
 function fetchRequest() {
