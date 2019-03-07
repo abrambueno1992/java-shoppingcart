2011f4625ffe08bee702400ad966e485ff90973e
Abraham Bueno <abueno@localhost.localdomain>
2019-03-07 11:47:06 -0600

    Changes with CRUD functionality

--- src/main/java/com/abrahambueno/javashoppingcart/repositories/SuppliersRepository.java
+++ src/main/java/com/abrahambueno/javashoppingcart/repositories/SuppliersRepository.java
@@ -2,6 +2,10 @@ package com.abrahambueno.javashoppingcart.repositories;
 
 import com.abrahambueno.javashoppingcart.models.Suppliers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SuppliersRepository extends JpaRepository<Suppliers, Long> {
+    @Query(value = "delete from supplierproducts where  supplierid = :supplierid", nativeQuery = true)
+    void deleteAllSupplierProducts(long supplierid);
+
 }
